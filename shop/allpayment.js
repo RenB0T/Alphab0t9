2e3fc555802624fe42bd7bdcae146ab70a093acc
RenB0T Official <94209612+RenB0T@users.noreply.github.com>
2021-12-19 11:32:59 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,29 +1,32 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT BY RenB0T
 
 *さ BANK*
 1. BRI 
-	ㅁ 5074-01-003141-52-8
-		• R***A*DI
+	ㅁ Tidak Tersedia
+		• RenB0T Official
 		
 *さ E-MONEY*
 1. GOPAY
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ Tidak Tersedia
+		• RenB0T Official
 2. OVO
-	ㅁ 0887 4350 47326
-		• EL*****TI
+	ㅁ Tidak Tersedia
+		• RenB0T Official
 3. DANA
-	ㅁ 0887 4350 47326
-		• ZE**** **C
+	ㅁ 0838 5370 7647
+		• RenB0T Official
 
-*さ PULSA*
-1. SMARTFREN
-	ㅁ 0887 4350 47326
-2. TELKOMSEL
-	ㅁ 0821 9311 5723
+*さ PULSA [ Di Utamakan ]*
+1. INDOSAT OOREDOO
+	ㅁ 0816 4935 7107
+2. AXIS
+	ㅁ 0838 5370 7647
+3. INDOSAT OOREDOO
+        ㅁ 0858 4582 3026
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+~• *Owner : [ wa.me/+6285845823026 ]*
 `
 	}
 
